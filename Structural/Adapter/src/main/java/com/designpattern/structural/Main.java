13e3d32c8a81c7003f44e54a33b9b87aa05c2db5
Ritvik Singh Chauhan <ritvik.singh.chauhan@gmail.com>
2023-02-12 13:57:20 +0530

    Updating Driver Classe for Adapter Design Pattern

--- Structural/Adapter/src/main/java/com/designpattern/structural/Main.java
+++ Structural/Adapter/src/main/java/com/designpattern/structural/Main.java
@@ -1,9 +1,35 @@
 package com.designpattern.structural;
 
+import com.designpattern.structural.adaptee.Nintendo;
+import com.designpattern.structural.adaptee.PlayStation;
+import com.designpattern.structural.adaptee.Xbox;
+import com.designpattern.structural.adapter.NintendoAdapter;
+import com.designpattern.structural.adapter.PlayStationAdapter;
+import com.designpattern.structural.adapter.XboxAdapter;
 import com.designpattern.structural.client.Client;
+import com.designpattern.structural.target.GamingConsole;
 
 public class Main {
     public static void main(String[] args) {
         Client client = new Client();
+
+        Xbox xbox = new Xbox();
+        GamingConsole xboxAdapter = new XboxAdapter(xbox);
+        client.setConsole(xboxAdapter);
+        client.playConsole();
+
+        System.out.println();
+
+        PlayStation playStation = new PlayStation();
+        GamingConsole playStationAdapter = new PlayStationAdapter(playStation);
+        client.setConsole(playStationAdapter);
+        client.playConsole();
+
+        System.out.println();
+
+        Nintendo nintendo = new Nintendo();
+        GamingConsole nintendoAdapter = new NintendoAdapter(nintendo);
+        client.setConsole(nintendoAdapter);
+        client.playConsole();
     }
 }
