0aa78ed58a80c85f4fd2f9df19c8bfd037c6ceb4
Ritvik Singh Chauhan <ritvik.singh.chauhan@gmail.com>
2023-02-12 13:47:30 +0530

    Adding Adaptee Classes for Adapter Design Pattern

---
+++ Structural/Adapter/src/main/java/com/designpattern/structural/adaptee/Nintendo.java
@@ -0,0 +1,10 @@
+package com.designpattern.structural.adaptee;
+
+public class Nintendo {
+    void nintendoInputReceive(){
+        System.out.println("Receiving Input for Nintendo");
+    }
+    void nintendoOutputDisplay(){
+        System.out.println("Displaying Output from Nintendo");
+    }
+}
---
+++ Structural/Adapter/src/main/java/com/designpattern/structural/adaptee/PlayStation.java
@@ -0,0 +1,10 @@
+package com.designpattern.structural.adaptee;
+
+public class PlayStation {
+    void playStationInputReceive(){
+        System.out.println("Receiving Input for PlayStation");
+    }
+    void playStationOutputDisplay(){
+        System.out.println("Displaying Output from PlayStation");
+    }
+}
