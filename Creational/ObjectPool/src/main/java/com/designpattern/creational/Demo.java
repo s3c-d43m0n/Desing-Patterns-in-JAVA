8dd15807082c338d6bba6007f3d83e90f1f552e1
Ritvik Singh Chauhan <ritvik.singh.chauhan@gmail.com>
2023-02-22 01:50:20 +0530

    Adding Driver Class for Demo

--- Creational/ObjectPool/src/main/java/com/designpattern/creational/Demo.java
+++ Creational/ObjectPool/src/main/java/com/designpattern/creational/Demo.java
@@ -5,7 +5,7 @@ import com.designpattern.creational.client.EmailSender;
 public class Demo {
     public static void main(String[] args){
         //Create Multiple Email Sender Threads
-        for(int i=0; i<20; i++){
+        for(int i=0; i<10; i++){
             new EmailSender(i).start();
         }
     }
